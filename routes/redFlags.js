1ff23e6eef407bb626482d18051024984b894166
boscoseries <boscoseries@gmail.com>
2018-11-28 22:01:47 +0100

    (feature) server: edit comment of red-flag [finishes #162284962]

--- routes/redFlags.js
+++ routes/redFlags.js
@@ -36,8 +36,50 @@ const records = [
 // Get all Red-flag records
 router.get("/red-flags", (req, res) => {
 	const record = records.filter(c => c.type === "red-flag");
-	if (!record) res.status(404).send("Record not a red-flag Entry. Try ../red-flags");
-	res.send(record);
+	if (!record) res.status(404).json({status: 404, error: "Not the correct path. Try checking /red-flags"});
+	res.status(200).json({status: 200, data: record});
+});
+
+// fetch a specific Red-flag records
+router.get("/red-flags/:id", (req, res) => {
+	const record = records.find(c => c.id === parseInt(req.params.id));
+	if (!record) res.status(404).json({status: 404, error: "Record not available"});
+	res.status(200).json({status: 200, data: record});
+});
+
+// Create a red flag record
+router.post("/red-flags", (req, res) => {
+	const record = {
+		id : records.length + 1, 
+		createdOn : Date.now,   
+		createdBy : records.length + 1, // represents the user who created this record 
+		type : "red-flag",       // [red-flag, intervention] 
+		location : req.body.location,   // Lat Long coordinates 
+		status : "draft",     // [draft, under investigation, resolved, rejected]  
+		comment : req.body.comment
+	};
+	records.push(record);
+	res.status(200).json({status: 200, data: record});
+});
+
+// edit the location of a red flag record
+router.put("/red-flags/:id/location", (req, res) => {
+	//look up the course and validate
+	const record = records.find(c => c.id === parseInt(req.params.id));
+	//if invalid return 404
+	if (!record || (record.type !== "red-flag")) res.status(404).json({status: 404, error: "Record not a red-flag Entry. check ./red-flags for entry types"});
+	record.location = req.body.location;
+	res.status(200).json({status: 200, data: record});
+});
+
+// edit the comment of a red flag record
+router.put("/red-flags/:id/comment", (req, res) => {
+	//look up the course and validate
+	const record = records.find(c => c.id === parseInt(req.params.id));
+	//if invalid return 404
+	if (!record || (record.type !== "red-flag")) res.status(404).json({status: 404, error: "Record not a red-flag Entry. check ./red-flags for entry types"});
+	record.comment = req.body.comment;
+	res.status(200).json({status: 200, data: record});
 });
 
 
