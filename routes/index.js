377f0f0aaca1d763f29ca2f8f27ce54b14d06f82
boscoseries <boscoseries@gmail.com>
2018-11-28 13:15:34 +0100

    (chore) TravisCI: Integrate travisCI [finishes #162266000]

---
+++ routes/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+//const records = require("../routes/records");
+
+const router = express.Router();
+
+const records = [
+	{ 
+		id : 1, 
+		createdOn : Date.now,   
+		createdBy : 1, // represents the user who created this record 
+		type : "red-flag",       // [red-flag, intervention] 
+		location : "Nigeria",   // Lat Long coordinates 
+		status : "Resolved",     // [draft, under investigation, resolved, rejected] 
+		comment : "My neighbour is a drug baron..." 
+	},
+	{ 
+		id : 2, 
+		createdOn : Date.now,   
+		createdBy : 2, // represents the user who created this record 
+		type : "intervention",       // [red-flag, intervention] 
+		location : "Ghana",   // Lat Long coordinates 
+		status : "Under investigation",     // [draft, under investigation, resolved, rejected] 
+		comment : "we need government to help us..." 
+	},
+	{ 
+		id : 3, 
+		createdOn : Date.now,   
+		createdBy : 3, // represents the user who created this record 
+		type : "red-flag",       // [red-flag, intervention] 
+		location : "Morocco",   // Lat Long coordinates 
+		status : "draft",     // [draft, under investigation, resolved, rejected]  
+		comment : "...abcde..." 
+	}
+	
+	
+];
+
+router.get("/", (req, res) => {
+	res.status(200).send(
+		"Welcome to my API"
+	);
+});
+
+router.get("/records", (req, res) => {
+	res.send(records);
+});
+
+
+module.exports = router;
